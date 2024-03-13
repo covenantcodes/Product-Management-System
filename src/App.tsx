37ba5de27922121a8b81e582b39d09f8256bf2be
Covenant Ifeoluwa <covenantifeoluwa4@gmail.com>
2024-03-13 23:26:55 +0100

    Added Login Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,10 @@
 import './App.css'
-import CustomButton from './Custom/Button/Button'
+import Login from './Auth/Login'
 
 const App = () => {
   return (
     <div>
-          <CustomButton
-            border="none"
-            color="white"
-            padding="1rem"
-            onClick={() => {console.log("Login")} }
-            radius="5px"
-            label="Login"
-            bgcolor="var(--primary-color)"
-            width="320px"
-            fontFamily="var(--main-font)"
-            fontSize="1rem"
-            marginTop="1rem"
-            cursor="pointer"
-          />
+          <Login/>
     </div>
   )
 }
--- src/Auth/Login.tsx
+++ src/Auth/Login.tsx
@@ -1,10 +1,102 @@
-
+import { useState } from "react";
+import "./Login.css";
+import "../Custom/Input/CustomInput.css";
+import CustomButton from "../Custom/Button/Button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye } from "@fortawesome/free-solid-svg-icons";
+import { faEyeSlash } from "@fortawesome/free-solid-svg-icons";
+// import loadingGif from "../../img/loader.gif";
 const Login = () => {
+  const [isEye, setIsEye] = useState(true);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  // const [message, setMessage] = useState("");
+  // const [isLoading, setIsLoading] = useState(false);
+
+  const handleIconClick = () => {
+    setIsEye((prevIsEye) => !prevIsEye);
+  };
+
+  const onChangeUsername = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const username = e.target.value;
+    setUsername(username);
+  };
+
+  const onChangePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const password = e.target.value;
+    setPassword(password);
+  };
   return (
     <div>
-      
-    </div>
-  )
-}
+      <div className="main-container">
+        <div className="login-container">
+          <div className="logo-container">
+            <img src="../../img/logo.png" alt="" />
+          </div>
+          <p className="error-text"></p>
+          <div className="custom-input">
+            <input
+              type="text"
+              placeholder="Username"
+              autoComplete="off"
+              onChange={onChangeUsername}
+              value={username}
+              required
+            />
+          </div>
 
-export default Login
+          <div className="password-input-container custom-input">
+            <input
+              type={isEye ? "password" : "text"}
+              placeholder="Password"
+              id="password"
+              onChange={onChangePassword}
+              value={password}
+              required
+            />
+            <div className="eyeIcon-container" onClick={handleIconClick}>
+              {isEye ? (
+                <FontAwesomeIcon className="eyeSlash" icon={faEyeSlash} />
+              ) : (
+                <FontAwesomeIcon className="eyeIcon" icon={faEye} />
+              )}
+            </div>
+          </div>
+
+          <div className="forgot-password-container">forgot password?</div>
+
+          <CustomButton
+            border="none"
+            color="white"
+            padding="1rem"
+            onClick={() => {
+              console.log("Login");
+            }}
+            radius="5px"
+            label="Login"
+            bgcolor="var(--primary-color)"
+            width="320px"
+            fontFamily="var(--main-font)"
+            fontSize="1rem"
+            marginTop="1rem"
+            cursor="pointer"
+          />
+
+          <div className="cta-register">
+            Don't have an account? <span>Register</span>{" "}
+          </div>
+        </div>
+
+        {/* Loading Modal */}
+        {/* {isLoading && (
+        <div className="loading-modal">
+          <div className="loading-spinner"></div>
+          <img src={loadingGif} alt="Loading...." />
+        </div>
+      )} */}
+      </div>
+    </div>
+  );
+};
+
+export default Login;
